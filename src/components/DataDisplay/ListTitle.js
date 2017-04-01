a0cb99e7a48de81076be257f4b33dd8079801d29
赵彬宇 <zhaobinyu@qijiayoudao.com>
2017-04-01 12:03:26 +0800

    ListTitle组件

---
+++ src/components/DataDisplay/ListTitle.js
@@ -0,0 +1,34 @@
+/**
+ * Created by Aus on 2017/4/1.
+ */
+import React from 'react'
+
+class ListTitle extends React.Component {
+    getClassName () {
+        let className = "list-title";
+        let {align} = this.props;
+
+        if(align){
+            className += " " + align;
+        }
+
+        return className;
+    }
+    render () {
+        let {title} = this.props;
+        const className = this.getClassName();
+
+        return (
+            <div className={className}>{title}</div>
+        )
+    }
+}
+
+// 用于展示列表最上方 列表数据的title
+// title: title展示名称 必填 字数最好不要太多 溢出文字会...显示
+// align: 对齐方式 可选值 枚举 "center" "right" 默认或者不填是左对齐
+ListTitle.propTypes = {
+    title: React.PropTypes.string.isRequired
+};
+
+export default ListTitle
--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,11 +1,12 @@
 import React from 'react'
+import ListTitle from '../components/DataDisplay/ListTitle'
 
 class Home extends React.Component {
     render () {
         return (
             <div className="home">
                 <h1 className="title">React 组件库</h1>
-
+                <ListTitle title="数据展示" />
             </div>
         )
     }
