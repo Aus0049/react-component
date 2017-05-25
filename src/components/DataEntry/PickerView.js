0c409146605eda73f9b376cdff5ceeb2d06df915
赵彬宇 <zhaobinyu@qijiayoudao.com>
2017-05-25 14:55:58 +0800

    使用zscroller实现滚动

--- src/components/DataEntry/PickerView.js
+++ src/components/DataEntry/PickerView.js
@@ -2,53 +2,22 @@
  * Created by Aus on 2017/5/24.
  */
 import React from 'react'
-import Hammer from 'hammerjs'
+import ZScroller from 'zscroller'
 
 // 选择器组件
 class PickerView extends React.Component {
     componentDidMount () {
-        this.bindTouchEvent();
-    }
-    bindTouchEvent () {
-        // 初次加载时候 初始化绑定事件
-        // 给list绑定事件
         let list = this.refs.list;
         let content = this.refs.content;
-        let listHammer = new Hammer(list);
-        // console.log(this.refs.content);
-        // window.content = this.refs.content;
-
-        let position = undefined;
-        // 拖动开始记录位置
-        listHammer.on('pan', function (e) {
-            position = e.deltaY;
-            // console.log("start");
+        // 最后还是用了何一鸣的zscroll插件
+        const zscroller = new ZScroller(content, {
+            scrollbars: false,
+            onScroll () {
+                // 回调函数
+                console.log(zscroller.scroller.getValues());
+            }
         });
-        listHammer.on('panmove', function (e) {
-            const distance = e.deltaY - position;
-            // console.log(distancen);
-            content.style.transform = `translateY(${distance}px)`;
-        });
-        listHammer.on('panend', function (e) {
-            console.log("end");
-        });
-
-        // listHammer.on('swipeleft', function (e) {
-        //     console.log("swipeleft");
-        // });
-
-        // this.bindPanDown(listHammer);
     }
-    // bindPanDown (hammer) {
-    //     // 绑定向下滑事件
-    //     hammer.on('panstart', (e)=>{
-    //         // 获取到滑动的距离
-    //         const distance = e.deltaY;
-    //         // 使content滑动对应距离
-    //         console.log(distance);
-    //         // this.refs.content.style.transform = `translateY(${distance}px)`;
-    //     });
-    // }
     render () {
 
         return (
