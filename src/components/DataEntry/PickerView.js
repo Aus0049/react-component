887334a39b1555440a37a4df7825ab871064a65d
赵彬宇 <zhaobinyu@qijiayoudao.com>
2017-05-25 15:56:26 +0800

    使用zscroller实现滚动

--- src/components/DataEntry/PickerView.js
+++ src/components/DataEntry/PickerView.js
@@ -7,16 +7,28 @@ import ZScroller from 'zscroller'
 // 选择器组件
 class PickerView extends React.Component {
     componentDidMount () {
-        let list = this.refs.list;
+        this.bindScrollEvent();
+    }
+    bindScrollEvent () {
+        // 绑定滚动的事件
         let content = this.refs.content;
         // 最后还是用了何一鸣的zscroll插件
-        const zscroller = new ZScroller(content, {
+        // 但是这个插件并没有太多的文档介绍 gg
+        // 插件demo地址：http://yiminghe.me/zscroller/examples/demo.html
+        let zscroller = new ZScroller(content, {
             scrollbars: false,
-            onScroll () {
-                // 回调函数
-                console.log(zscroller.scroller.getValues());
+            scrollingX: false,
+            snapping: true, // 滚动结束之后 滑动对应的位置
+            penetrationDeceleration: .1,
+            minVelocityToKeepDecelerating: 0.5,
+            scrollingComplete () {
+                // 滚动结束 回调
+                console.log(11);
             }
         });
+
+        // 设置每个格子的高度 这样滚动结束 自动滚到对应格子上
+        zscroller.scroller.setSnapSize(0, 68);
     }
     render () {
 
@@ -24,7 +36,7 @@ class PickerView extends React.Component {
             <div className="zby-picker-view-box">
                 <div className="zby-picker-view-item">
                     <div className="zby-picker-view-list">
-                        <div className="zby-picker-view-window" ref="list"></div>
+                        <div className="zby-picker-view-window"></div>
                         <div className="zby-picker-view-indicator"></div>
                         <div className="zby-picker-view-content" ref="content">
                             <div className="zby-picker-view-col selected">1</div>
