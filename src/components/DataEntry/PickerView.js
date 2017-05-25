64c7d6a02dfddfe06a60a24648e2e8b1c8c3b077
赵彬宇 <zhaobinyu@qijiayoudao.com>
2017-05-25 10:27:32 +0800

    考虑使用什么手势插件 筛选中

--- src/components/DataEntry/PickerView.js
+++ src/components/DataEntry/PickerView.js
@@ -2,31 +2,38 @@
  * Created by Aus on 2017/5/24.
  */
 import React from 'react'
+import Hammer from 'react-hammerjs'
 
 // 选择器组件
 class PickerView extends React.Component {
     componentDidMount () {
         // 初次加载时候 初始化绑定事件
     }
+    handleTap () {
+        console.log(arguments);
+    }
     render () {
+        let {handleTap} = this;
         return (
             <div className="zby-picker-view-box">
                 <div className="zby-picker-view-item">
                     <div className="zby-picker-view-list">
-                        <div className="zby-picker-view-window"></div>
-                        <div className="zby-picker-view-indicator"></div>
-                        <div className="zby-picker-view-content">
-                            <div className="zby-picker-view-col selected">1</div>
-                            <div className="zby-picker-view-col">2</div>
-                            <div className="zby-picker-view-col">3</div>
-                            <div className="zby-picker-view-col">4</div>
-                            <div className="zby-picker-view-col">5</div>
-                            <div className="zby-picker-view-col">6</div>
-                            <div className="zby-picker-view-col">7</div>
-                            <div className="zby-picker-view-col">8</div>
-                            <div className="zby-picker-view-col">9</div>
-                            <div className="zby-picker-view-col">10</div>
-                        </div>
+                        <Hammer onTap={handleTap}>
+                            <div className="zby-picker-view-window"></div>
+                            <div className="zby-picker-view-indicator"></div>
+                            <div className="zby-picker-view-content">
+                                <div className="zby-picker-view-col selected">1</div>
+                                <div className="zby-picker-view-col">2</div>
+                                <div className="zby-picker-view-col">3</div>
+                                <div className="zby-picker-view-col">4</div>
+                                <div className="zby-picker-view-col">5</div>
+                                <div className="zby-picker-view-col">6</div>
+                                <div className="zby-picker-view-col">7</div>
+                                <div className="zby-picker-view-col">8</div>
+                                <div className="zby-picker-view-col">9</div>
+                                <div className="zby-picker-view-col">10</div>
+                            </div>
+                        </Hammer>
                     </div>
                 </div>
             </div>
