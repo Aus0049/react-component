b610f71be7908fe4ac8e1b4ddf6d34c5bdca0c45
赵彬宇 <zhaobinyu@qijiayoudao.com>
2017-05-26 11:27:18 +0800

    picker view column罗列及滚动完成

--- src/components/DataEntry/PickerColumn.js
+++ src/components/DataEntry/PickerColumn.js
@@ -2,40 +2,48 @@
  * Created by Aus on 2017/5/25.
  */
 import React from 'react'
+import ZScroller from 'zscroller'
+import classNames from 'classnames'
 
 // picker-view 中的列
 class PickerColumn extends React.Component {
-    constructor (props) {
-        super(props);
-        this.state = {
-            value: undefined
-        }
-    }
     componentDidMount () {
-        // 初次加载时候 不受控组件 存入初始值
-        this.setState({
-            value: this.props.value
-        });
+        // 绑定事件
+        this.bindScrollEvent();
         // 列表滚到对应位置
+    }
+    bindScrollEvent () {
+        // 绑定滚动的事件
+        let content = this.refs.content;
+        // getBoundingClientRect js原生方法
+        const height = this.refs.indicator.getBoundingClientRect().height;
+
+        // 最后还是用了何一鸣的zscroll插件
+        // 但是这个插件并没有太多的文档介绍 gg
+        // 插件demo地址：http://yiminghe.me/zscroller/examples/demo.html
+        this.zscroller = new ZScroller(content, {
+            scrollbars: false,
+            scrollingX: false,
+            snapping: true, // 滚动结束之后 滑动对应的位置
+            penetrationDeceleration: .1,
+            minVelocityToKeepDecelerating: 0.5,
+            scrollingComplete () {
+                // 滚动结束 回调
+                console.log(11);
+            }
+        });
+
+        // 设置每个格子的高度 这样滚动结束 自动滚到对应格子上
+        // 单位必须是px 所以要动态取一下
+        this.zscroller.scroller.setSnapSize(0, height);
     }
     getCols () {
         // 根据value 和 index 获取到对应的data
         let {data, value, index} = this.props;
         let result = [];
 
-        const dataArray = this.getDataArray(data, value, index);
-
-        return result;
-    }
-    getDataArray (data, value, index) {
-        let result = [];
-
-        if(index == 0){
-            for(let i of data){
-                result.push({label: i.label, value: i.value});
-            }
-        } else {
-
+        for(let i = 0; i < data.length; i++){
+            result.push(<div key={index + "-" + i} className={classNames(['zby-picker-view-col', {'selected': data[i].value == value}])}>{data[i].label}</div>);
         }
 
         return result;
@@ -47,19 +55,9 @@ class PickerColumn extends React.Component {
             <div className="zby-picker-view-item">
                 <div className="zby-picker-view-list">
                     <div className="zby-picker-view-window"></div>
-                    <div className="zby-picker-view-indicator"></div>
+                    <div className="zby-picker-view-indicator" ref="indicator"></div>
                     <div className="zby-picker-view-content" ref="content">
                         {cols}
-                        <div className="zby-picker-view-col selected">1</div>
-                        <div className="zby-picker-view-col">2</div>
-                        <div className="zby-picker-view-col">3</div>
-                        <div className="zby-picker-view-col">4</div>
-                        <div className="zby-picker-view-col">5</div>
-                        <div className="zby-picker-view-col">6</div>
-                        <div className="zby-picker-view-col">7</div>
-                        <div className="zby-picker-view-col">8</div>
-                        <div className="zby-picker-view-col">9</div>
-                        <div className="zby-picker-view-col">10</div>
                     </div>
                 </div>
             </div>
--- src/components/DataEntry/PickerView.js
+++ src/components/DataEntry/PickerView.js
@@ -2,47 +2,24 @@
  * Created by Aus on 2017/5/24.
  */
 import React from 'react'
-import ZScroller from 'zscroller'
 import PickerColumn from './PickerColumn'
 
 // 选择器组件
 class PickerView extends React.Component {
     componentDidMount () {
-        // this.bindScrollEvent();
-    }
-    bindScrollEvent () {
-        // 绑定滚动的事件
-        let content = this.refs.content;
-        // 最后还是用了何一鸣的zscroll插件
-        // 但是这个插件并没有太多的文档介绍 gg
-        // 插件demo地址：http://yiminghe.me/zscroller/examples/demo.html
-        let zscroller = new ZScroller(content, {
-            scrollbars: false,
-            scrollingX: false,
-            snapping: true, // 滚动结束之后 滑动对应的位置
-            penetrationDeceleration: .1,
-            minVelocityToKeepDecelerating: 0.5,
-            scrollingComplete () {
-                // 滚动结束 回调
-                console.log(11);
-            }
-        });
 
-        // 设置每个格子的高度 这样滚动结束 自动滚到对应格子上
-        zscroller.scroller.setSnapSize(0, 68);
     }
     getColumns () {
-        // let result = [];
+        let result = [];
         let {col, data, value} = this.props;
 
-        const result = this.getColumnsData(data, value, [], 0);
-        console.log(result);
+        let array = this.getColumnsData(data, value, [], 0);
 
-        // for(let i = 0; i < col; i++){
-        //     result.push(<PickerColumn value={value} data={data} index={i}/>);
-        // }
-        //
-        // return result;
+        for(let i = 0; i < col; i++){
+            result.push(<PickerColumn key={i} value={value[i]} data={array[i]} index={i}/>);
+        }
+
+        return result;
     }
     getColumnsData (tree, value, hasFind, deep) {
         // 遍历tree
