e8d532a379e78965c43c3d29fadaf82bb7597a26
赵彬宇 <zhaobinyu@qijiayoudao.com>
2017-06-02 21:11:03 +0800

    date模式下的时间数据完成制作

--- src/components/DataEntry/DatePicker.js
+++ src/components/DataEntry/DatePicker.js
@@ -8,9 +8,9 @@ import Touchable from 'rc-touchable'
 import moment from 'moment'
 
 const monthArray = [
-    {label: "1月", value: 0},{label: "2月", value: 1},{label: "3月", value: 2},{label: "4月", value: 3},
-    {label: "5月", value: 4},{label: "6月", value: 5},{label: "7月", value: 6},{label: "8月", value: 7},
-    {label: "9月", value: 8},{label: "10月", value: 9},{label: "11月", value: 10},{label: "12月", value: 11}
+    {label: "1月", value: "0"},{label: "2月", value: "1"},{label: "3月", value: "2"},{label: "4月", value: "3"},
+    {label: "5月", value: "4"},{label: "6月", value: "5"},{label: "7月", value: "6"},{label: "8月", value: "7"},
+    {label: "9月", value: "8"},{label: "10月", value: "9"},{label: "11月", value: "10"},{label: "12月", value: "11"}
 ];
 
 // 日期时间选择器
@@ -40,6 +40,27 @@ class DatePicker extends React.Component {
             defaultValue: value,
             selectedValue: value
         });
+    }
+    handlePickerViewChange (newValue) {
+        // picker view回调的时候
+        const {mode} = this.props;
+
+        switch (mode) {
+            case "date":
+                // 检验新日期是否合法
+                // 年月一定合法 主要就是检验日
+                const newMaxDate = this.checkDaysByYearMonth(moment(newValue[0] + "-" + (newValue[1] + 1), "YYYY-MM"));
+
+                if(newValue[3] > newMaxDate){
+                    newValue[3] = newMaxDate + "";
+                }
+
+                let newMoment = moment([Number.parseInt(newValue[0]), Number.parseInt(newValue[1]), Number.parseInt(newValue[2])]);
+                this.setState({
+                    selectedValue: newMoment
+                });
+                break;
+        }
     }
     handleClickOpen (e) {
 
@@ -97,13 +118,14 @@ class DatePicker extends React.Component {
     }
     checkDaysByYearMonth (value) {
         const month = value.month();
+
         // 判断大小月
-        if([1,3,5,7,8,10,12].indexOf(month) >= 0){
+        if([0,2,4,6,7,9,11].indexOf(month) >= 0){
             // 大月 31天
             return 31;
-        } else if ([4,6,9,11].indexOf(month) >= 0) {
+        } else if ([3,5,8,10].indexOf(month) >= 0) {
             // 小月 30天
-            return 31;
+            return 30;
         } else {
             // 2月 判断是否闰年
             if(moment([value.year()]).isLeapYear()){
@@ -117,7 +139,7 @@ class DatePicker extends React.Component {
     getDateByMode (mode) {
         let result = [];
         let todayMoment = moment();
-        let {value} = this.props;
+        let {selectedValue} = this.state;
 
         switch (mode) {
             case "date":
@@ -127,15 +149,15 @@ class DatePicker extends React.Component {
                 let currentYear = Number.parseInt(todayMoment.format("YYYY"));
 
                 for(let i = currentYear - 5; i < currentYear + 5; i++){
-                    yearArray.push({label: i + "年", value: i});
+                    yearArray.push({label: i + "年", value: i + ''});
                 }
 
                 // 准备日 根据值判断当月有多少天
-                const daysMax = this.checkDaysByYearMonth(value);
+                const daysMax = this.checkDaysByYearMonth(selectedValue);
 
                 let dayArray = [];
                 for(let i = 1; i < daysMax + 1; i++){
-                    dayArray.push({label: i + "日", value: i});
+                    dayArray.push({label: i + "日", value: i + ''});
                 }
 
                 result = [yearArray, monthArray, dayArray];
@@ -148,9 +170,21 @@ class DatePicker extends React.Component {
         // 根据mode不同 准备不同数据
         // date picker中的picker view 应该是不级联
         const {mode} = this.props;
+        const {selectedValue, show} = this.state;
 
-        const data = this.getDateByMode(mode);
-        console.log(data);
+        if(selectedValue != undefined && show){
+            const data = this.getDateByMode(mode);
+
+            if(mode == "date"){
+                return <PickerView
+                    col={3}
+                    data={data}
+                    value={[selectedValue.year() + '', selectedValue.month() + '', selectedValue.date() + '']}
+                    cascade={false}
+                    onChange={this.handlePickerViewChange.bind(this)}>
+                </PickerView>;
+            }
+        }
     }
     getPopupDOM () {
         const {show, animation} = this.state;
