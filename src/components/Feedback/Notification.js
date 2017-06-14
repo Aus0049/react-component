8abde64fe77d44dcf5e9229dfeb3c0ff8825363f
赵彬宇 <zhaobinyu@qijiayoudao.com>
2017-06-14 20:04:23 +0800

    notification 组件新增重写方法

--- src/components/Feedback/Notification.js
+++ src/components/Feedback/Notification.js
@@ -2,13 +2,12 @@
  * Created by Aus on 2017/6/14.
  */
 
-/**
- *
- * Notification是Notice上层组件
- * 是动态插入和删除DOM节点的核心
- * 同时也向上提供给Toast很多核心函数
- * */
+
+// Notification是Notice上层组件
+// 是动态插入和删除DOM节点的核心
+// 同时也向上提供给Toast很多核心函数
 import React from 'react'
+import ReactDOM from 'react-dom'
 import Notice from './Notice'
 
 class Notification extends React.Component {
@@ -68,11 +67,41 @@ class Notification extends React.Component {
     }
 }
 
-//
+// 统计notice总数 防止重复
 let noticeNumber = 0;
 
 const getUuid = () => {
     return "notification-" + new Date() + "-" + noticeNumber++;
+};
+
+// Notification增加一个重写方法
+// 该方法方便Notification组件动态添加到页面中和重写
+Notification.reWrite = function (properties) {
+    const { getContainer, ...props } = properties || {};
+
+    let div;
+    if (getContainer) {
+        div = getContainer();
+    } else {
+        div = document.createElement('div');
+        document.body.appendChild(div);
+    }
+
+    const notification = ReactDOM.render(<Notification {...props} />, div);
+
+    return {
+        notice(noticeProps) {
+            notification.add(noticeProps);
+        },
+        removeNotice(key) {
+            notification.remove(key);
+        },
+        component: notification,
+        destroy() {
+            ReactDOM.unmountComponentAtNode(div);
+            document.body.removeChild(div);
+        }
+    }
 };
 
 export default Notification
