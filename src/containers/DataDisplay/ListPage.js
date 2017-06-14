590c17707a78278ec3078b6d5357e306db193939
赵彬宇 <zhaobinyu@qijiayoudao.com>
2017-06-14 11:58:57 +0800

    更新部分页面组件为无状态组件

--- src/containers/DataDisplay/ListPage.js
+++ src/containers/DataDisplay/ListPage.js
@@ -4,55 +4,45 @@
 import React from 'react'
 import ListTitle from '../../components/DataDisplay/ListTitle'
 import List from '../../components/DataDisplay/List'
-import ReactCSSTransitionGroup from 'react-addons-css-transition-group'
 import Tools from '../../components/Tools/Tools'
 
 const Item = List.Item;
 
-class ListPage extends React.Component {
-    render () {
-        return (
-            <ReactCSSTransitionGroup
-                component="div"
-                className="page list"
-                transitionName="todo"
-                transitionAppear={true}
-                transitionAppearTimeout={500}
-                transitionEnterTimeout={500}
-                transitionLeaveTimeout={500}>
-                <h1 className="title">
-                    <i className="fa fa-home" onClick={()=>{Tools.linkTo("/index")}}></i>
-                    List
-                </h1>
+const ListPage = () => {
+    return (
+        <div className="page list">
+            <h1 className="title">
+                <i className="fa fa-home" onClick={()=>{Tools.linkTo("/index")}}></i>
+                List
+            </h1>
 
-                <ListTitle title="基本" />
-                <List>
-                    <Item>基本标题</Item>
-                </List>
+            <ListTitle title="基本" />
+            <List>
+                <Item>基本标题</Item>
+            </List>
 
-                <ListTitle title="添加icon" />
-                <List>
-                    <Item icon="horizontal">基本标题</Item>
-                </List>
-                <ListTitle title="添加副标题" />
-                <List>
-                    <Item subtitle="副标题在右" icon="horizontal">基本标题</Item>
-                    <Item subtitle="副标题在下" icon="vertical" multipleLine>基本标题</Item>
-                </List>
+            <ListTitle title="添加icon" />
+            <List>
+                <Item icon="horizontal">基本标题</Item>
+            </List>
+            <ListTitle title="添加副标题" />
+            <List>
+                <Item subtitle="副标题在右" icon="horizontal">基本标题</Item>
+                <Item subtitle="副标题在下" icon="vertical" multipleLine>基本标题</Item>
+            </List>
 
-                <ListTitle title="点击事件" />
-                <List>
-                    <Item subtitle="请点击" icon="horizontal" onClick={()=>{alert("点击成功")}}>基本标题</Item>
-                    <Item subtitle="请长按" icon="horizontal" onLongPress={()=>{alert("长按成功")}}>基本标题</Item>
-                </List>
+            <ListTitle title="点击事件" />
+            <List>
+                <Item subtitle="请点击" icon="horizontal" onClick={()=>{alert("点击成功")}}>基本标题</Item>
+                <Item subtitle="请长按" icon="horizontal" onLongPress={()=>{alert("长按成功")}}>基本标题</Item>
+            </List>
 
-                <ListTitle title="禁止点击" />
-                <List>
-                    <Item subtitle="副标题在右" icon="horizontal" disabled>基本标题</Item>
-                </List>
-            </ReactCSSTransitionGroup>
-        )
-    }
-}
+            <ListTitle title="禁止点击" />
+            <List>
+                <Item subtitle="副标题在右" icon="horizontal" disabled>基本标题</Item>
+            </List>
+        </div>
+    )
+};
 
 export default ListPage
