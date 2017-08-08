fa946b5daa0b149aa1745791fa66b22cffe7579e
赵彬宇 <zhaobinyu@qijiayoudao.com>
2017-08-08 22:03:34 +0800

    Form组件增加报错实现

--- src/components/Form/components/Func.js
+++ src/components/Form/components/Func.js
@@ -3,9 +3,12 @@
  */
 // 表单添加报错
 export function showError (errorText) {
-
+    const errorBox = document.createElement('div');
+    errorBox.className = 'zby-form-error';
+    errorBox.innerText = errorText;
+    document.body.appendChild(errorBox);
 }
 
 export function clearError () {
-
+    document.querySelector('.zby-form-error').remove();
 }
