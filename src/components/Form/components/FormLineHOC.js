cf5a8c948e689e059eac40ff9d550305114b4e36
赵彬宇 <zhaobinyu@qijiayoudao.com>
2017-12-01 16:16:18 +0800

    使用HOC重写input组件

---
+++ src/components/Form/components/FormLineHOC.js
@@ -0,0 +1,27 @@
+/**
+ * Created by Aus on 2017/12/1.
+ */
+import React from 'react'
+import classNames from 'classnames'
+import Icon from 'component-font-awesome'
+import '../style/form.scss'
+
+// 生成表单的HOC
+export default function FormLineHOC(WrappedComponent) {
+    // 反向继承
+    return class FormLine extends WrappedComponent {
+        render () {
+            const {prefixCls, labelName, errorText, required} = this.props;
+
+            return (
+                <div className={classNames([prefixCls, {error: !!errorText}])}>
+                    <div className="title">
+                        <Icon type="asterisk" className={required ? 'required' : null} />
+                        <div className="label-name">{labelName}</div>
+                    </div>
+                    {super.render()}
+                </div>
+            );
+        }
+    }
+}
--- src/components/Form/components/Input.js
+++ src/components/Form/components/Input.js
@@ -4,7 +4,7 @@
 import React from 'react'
 import classNames from 'classnames'
 import Icon from 'component-font-awesome'
-import '../style/form.scss'
+import FormLineHOC from './FormLineHOC'
 
 const feedbackIconMap = {
     success: 'check-circle',
@@ -41,7 +41,7 @@ class Input extends React.Component {
     }
     render () {
         const {stateValue} = this.state;
-        const {prefixCls, required, labelName, value, readOnly, placeHolder, controlled, kind, errorText, feedbackIcon} = this.props;
+        const {value, readOnly, placeHolder, controlled, kind, feedbackIcon} = this.props;
 
         let inputType;
 
@@ -61,28 +61,22 @@ class Input extends React.Component {
         }
 
         return (
-            <div className={classNames([prefixCls, {error: !!errorText}])}>
-                <div className="title">
-                    <Icon type="asterisk" className={required ? 'required' : null} />
-                    <div className="label-name">{labelName}</div>
-                </div>
-                <div className={classNames(['content', {'has-feedback': feedbackIcon}])}>
-                    {readOnly ?
-                        <p className="input-readonly">{value ? value : placeHolder}</p>
-                        : controlled ?
-                            <input
-                                type={inputType} value={value}
-                                placeholder={placeHolder} onChange={this.handleChange}
-                            />
-                            :
-                            <input
-                                type={inputType} value={stateValue}
-                                onChange={this.handleChange}
-                                placeholder={placeHolder}
-                            />
-                    }
-                    {!readOnly && feedbackIcon ? <Icon type={feedbackIconMap[feedbackIcon]} /> : ''}
-                </div>
+            <div className={classNames(['content', {'has-feedback': feedbackIcon}])}>
+                {readOnly ?
+                    <p className="input-readonly">{value ? value : placeHolder}</p>
+                    : controlled ?
+                        <input
+                            type={inputType} value={value}
+                            placeholder={placeHolder} onChange={this.handleChange}
+                        />
+                        :
+                        <input
+                            type={inputType} value={stateValue}
+                            onChange={this.handleChange}
+                            placeholder={placeHolder}
+                        />
+                }
+                {!readOnly && feedbackIcon ? <Icon type={feedbackIconMap[feedbackIcon]} /> : ''}
             </div>
         );
     }
@@ -117,115 +111,4 @@ Input.defaultProps = {
     onChange: empty
 };
 
-export default Input;
-// import React from 'react'
-// import classNames from 'classnames'
-// import {showError, clearError} from '../util'
-// import '../style/index.scss'
-//
-// const feedbackIconMap = {
-//     success: 'check-circle',
-//     warning: 'exclamation-circle',
-//     wrong: 'times-circle',
-//     loading: 'circle-o-notch fa-spin'
-// };
-//
-// const Input = (props) => {
-//     const {required, labelName, readOnly, controlled, value, placeHolder, kind, error, onChange, feedbackIcon} = props;
-//
-//     function handleChange(e) {
-//         onChange({value: e.target.value});
-//     }
-//
-//     function handleFocus() {
-//         if (!!error) {
-//             showError(error);
-//         }
-//     }
-//
-//     function handleBlur() {
-//         if (!!error) {
-//             clearError(error);
-//         }
-//     }
-//
-//     let inputType;
-//
-//     switch (kind) {
-//         case 'number':
-//             inputType = 'number';
-//             break;
-//         case 'phone':
-//             inputType = 'number';
-//             break;
-//         case 'password':
-//             inputType = 'password';
-//             break;
-//         default:
-//             inputType = 'text';
-//             break;
-//     }
-//
-//     return (
-//         <div className={classNames(['zby-form-line-box', {'error': !!error}])}>
-//             <div className="title">
-//                 <i className={classNames(['fa', 'fa-asterisk', {required: required}])}/>
-//                 <div className="label-name">{labelName}</div>
-//             </div>
-//             <div className={classNames(['content', {'has-feedback': feedbackIcon}])}>
-//                 {readOnly ?
-//                     <p className="input-readonly">{value ? value : placeHolder}</p>
-//                     : controlled ?
-//                         <input
-//                             type={inputType} value={value}
-//                             placeholder={placeHolder} onChange={handleChange}
-//                             onFocus={handleFocus} onBlur={handleBlur}
-//                         />
-//                         :
-//                         <input
-//                             type={inputType} defaultValue={value}
-//                             placeholder={placeHolder} onFocus={handleFocus}
-//                             onBlur={handleBlur}
-//                         />
-//                 }
-//                 {!readOnly && feedbackIcon ?
-//                     <span className={`fa fa-${feedbackIconMap[feedbackIcon]}`}/>
-//                     :
-//                     ''
-//                 }
-//             </div>
-//         </div>
-//     )
-// };
-//
-// function empty() {
-// }
-//
-// Input.PropTypes = {
-//     labelName: React.PropTypes.string.isRequired,
-//     prefixCls: React.PropTypes.string, // 前缀class
-//     required: React.PropTypes.bool,
-//     readOnly: React.PropTypes.bool,
-//     controlled: React.PropTypes.bool,
-//     value: React.PropTypes.oneOfType([React.PropTypes.string, React.PropTypes.number]),
-//     placeHolder: React.PropTypes.string,
-//     kind: React.PropTypes.oneOf(['password', 'phone', 'email', 'number', 'text']),
-//     error: React.PropTypes.string,
-//     feedbackIcon: React.PropTypes.oneOf(['success', 'warning', 'error', 'loading', '']),
-//     onChange: React.PropTypes.func,
-// };
-//
-// Input.defaultProps = {
-//     prefixCls: 'zby-form-input',
-//     required: false,
-//     readOnly: false,
-//     controlled: true,
-//     value: '',
-//     placeHolder: '',
-//     kind: 'text',
-//     error: '',
-//     feedbackIcon: '',
-//     onChange: empty
-// };
-//
-// export default Input;
+export default FormLineHOC(Input);
